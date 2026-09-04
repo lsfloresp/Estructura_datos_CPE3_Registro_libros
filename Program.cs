eac453f15aabcb2f485aa719a2809171e3bd087b
lsfloresp <ls.floresp@uea.edu.ec>
2026-09-03 23:25:08 -0500

    Conectar registro de libros con la biblioteca

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Biblioteca biblioteca = new Biblioteca();
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("          REGISTRO DE LIBRO");
+Console.WriteLine("----------------------------------------");
+
+Libro nuevoLibro = new Libro();
+
+Console.Write("Ingrese el código del libro: ");
+nuevoLibro.Codigo = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el título del libro: ");
+nuevoLibro.Titulo = Console.ReadLine();
+
+Console.Write("Ingrese el autor del libro: ");
+nuevoLibro.Autor = Console.ReadLine();
+
+Console.Write("Ingrese la categoría del libro: ");
+nuevoLibro.Categoria = Console.ReadLine();
+
+Console.Write("Ingrese el número de ejemplares: ");
+nuevoLibro.Ejemplares = int.Parse(Console.ReadLine());
+
+biblioteca.RegistrarLibro(nuevoLibro);
