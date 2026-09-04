00ce60799fd39a5a2cea0990d0fee16bf15e029f
lsfloresp <ls.floresp@uea.edu.ec>
2026-09-03 23:10:27 -0500

    Crear clase Libro

---
+++ Libro.cs
@@ -0,0 +1,8 @@
+public class Libro
+{
+    public int Codigo { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Categoria { get; set; }
+    public int Ejemplares { get; set; }
+}
